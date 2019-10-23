166ed90ce5293390a995254efccaba720e33cbfc
Corné Visser <cvisser@itpartner.nl>
2019-10-23 10:35:57 +0200

    WIP

---
+++ Project-Starlord/Data/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project_Starlord.Models;
+
+namespace Project_Starlord.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+
+        }
+        DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ Project-Starlord/Data/UserContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Project_Starlord.Models;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace Project_Starlord.Data
+{
+    public class UserContext : DbContext, IDbContext
+    {
+        public UserContext(Microsoft.EntityFrameworkCore.DbContextOptions options) : base(options)
+        {
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<UserModel> Users { get; set; }
+        public DbContextConfiguration Configuration { get; }
+
+        public int SaveChanges()
+        {
+            return 0;
+        }
+
+        public int CurrentUserId { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        //}
+    }
+}
