1084189e84d92cf7caf8058bcb7bc02df0e7bae5
Corné Visser <cvisser@itpartner.nl>
2019-10-26 21:28:26 +0200

    WIP

---
+++ Project-Starlord/Data/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Project_Starlord.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Project_Starlord.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() { }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
