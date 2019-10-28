2abb9494e5b14c4fd260482955c16f8776f05461
Corné Visser <cvisser@itpartner.nl>
2019-10-28 11:37:53 +0100

    Added password hash method Added that we check the hashed password while logging in

--- Project-Starlord/Helpers/ExtensionMethods.cs
+++ Project-Starlord/Helpers/ExtensionMethods.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using Project_Starlord.Models;
 
 namespace Project_Starlord.Helpers
@@ -15,6 +17,22 @@ namespace Project_Starlord.Helpers
         public static UserModel WithoutPassword(this UserModel user)
         {
             user.Password = null;
+            return user;
+        }
+
+        public static UserModel HashPassword(this UserModel user) {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(user.Password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            user.Password = Convert.ToBase64String(hashBytes);
+
             return user;
         }
     }
