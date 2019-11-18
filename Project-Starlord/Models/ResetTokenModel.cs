8de8f7136988b8195d31266349300cb29a38b94d
MrTheGood <maarten@degoedeweb.nl>
2019-11-18 02:59:19 +0100

    Add reset token generation while not really knowing what i'm doing

--- Project-Starlord/Models/ResetTokenModel.cs
+++ Project-Starlord/Models/ResetTokenModel.cs
@@ -8,21 +8,20 @@ using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 
 namespace Project_Starlord.Models
 {
-    public class UserModel
+    public class ResetTokenModel
     {
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        public string Username { get; set; }
-
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
+        [Required]
         public string Token { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime Expires { get; set; }
+
+        public bool IsTokenUsed { get; set; }
     }
 }
